87205a6b5e70fd5e48b436910b5d1b5756887383
Andrea del Rio <adelrio@gmail.com>
2017-03-13 10:40:52 -0700

    update store to include currentUser, update session/root reducers

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,9 +1,9 @@
 import {combineReducers} from 'redux';
-
+import SessionReducer from './session_reducer';
 // import BenchesReducer from './benches_reducer';
 
 const RootReducer = combineReducers({
-
+  session: SessionReducer
 });
 
 export default RootReducer;
