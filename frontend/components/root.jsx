f318201e7aae1320bbac25ffaf53d504e237f892
Andrea del Rio <adelrio@gmail.com>
2017-03-13 15:16:26 -0700

    session/session container/form

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import SessionFormContainer from './session_form/session_form_container';
 import App from './app';
 
 const Root = ({ store }) => (
   <Provider store={ store }>
     <Router history={ hashHistory }>
       <Route path="/" component={ App }/>
+      <Route path="/login" component={ SessionFormContainer }></Route>
+      <Route path="/signup" component={ SessionFormContainer }></Route>
     </Router>
   </Provider>
 );
